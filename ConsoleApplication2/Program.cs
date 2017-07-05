3952f7075110e1d4f5ae4ac596aa95569a07d59f
Barkah Martiyana Ramdani <barkahmr@gmail.com>
2017-07-05 10:01:08 +0700

    Add project files.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int panjang;
+            int lebar;
+
+            Console.Write("Masukan panjang:");
+            panjang = int.Parse(Console.ReadLine());
+
+            Console.Write("Masukan lebar: ");
+            lebar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("Luas persegi panjang: " + panjang * lebar);
+            Console.WriteLine("Keliling persegi panjang: "  (2*panjang + 2*lebar) );
+            Console.WriteLine("------------------");
+
+            Console.ReadLine();
+        }
+    }
+}
